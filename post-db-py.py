f1df7179da25637820a54315a5ac3dc8395f9499
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2022-04-04 15:34:24 -0500

    seeding sales table pt1

--- post-db-py.py
+++ post-db-py.py
@@ -4,5 +4,23 @@ config = dotenv_values(".env")
 myUser = config['USER_NAME']
 myPassword = config['PASS_WORD']
 
+#----------------------------------------------------------------
+connect = psycopg2.connect(database = "py-lab", user = myUser, password = myPassword, host="localhost", port="5432")
 
-psycopg2.connect(dataname = "py-lab", user = myUser, password = myPassword)
+cursor = connect.cursor()
+
+
+cursor.execute('''DROP TABLE IF EXISTS sales''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS sales
+               (ORDER_NUM INT PRIMARY KEY,
+               ORDER_TYPE TEXT NOT NULL,
+               CUST_NAME TEXT NOT NULL,
+               PROD_NUM TEXT,
+               PROD_NAME TEXT,               
+               QUANTITY INT NOT NULL,
+               PRICE REAL,
+               DISCOUNT REAL,
+               ORDER_TOTAL REAL);''')
+
+connect.commit()
+connect.close()
